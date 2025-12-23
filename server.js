acf510763588156d95f204d8facbeab0aec04e69
Yehouda-12 <coheny748@gmail.com>
2025-12-23 13:00:45 +0200

    Remove offensive response for '/yehiel' endpoint

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/users',users)
-app.get('/yehiel', (req,res)=>{
-  res.send("FUCK YOU YEHIEL")})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
