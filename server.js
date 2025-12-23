7634808f4edddc5641fe144e385c83ebdb3456cc
Yehouda-12 <coheny748@gmail.com>
2025-12-23 12:50:20 +0200

    Add endpoint to respond to '/yehiel'

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use((req, res, next) => {
 });
 
 app.use('/users',users)
+app.get('/yehiel', (req,res)={
+  res.send('FUCK YOU YEHIEL')})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
